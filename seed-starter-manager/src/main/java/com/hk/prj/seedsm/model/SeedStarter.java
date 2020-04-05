817f7c4a347357ad6af536922caae99b75dee4ec
kumarhemant10 <hkchauhan022@gmail.com>
2020-04-05 22:42:29 +0530

    seed starter application added

---
+++ seed-starter-manager/src/main/java/com/hk/prj/seedsm/model/SeedStarter.java
@@ -0,0 +1,26 @@
+package com.hk.prj.seedsm.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class SeedStarter extends BaseEntity{
+
+	
+	private LocalDate datePlanted;
+	private Boolean covered;
+	private String type;
+	private String[] features;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Row> rows= new ArrayList<>();
+	
+}
